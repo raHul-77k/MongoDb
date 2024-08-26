69442d65b48e5dccc6a4ac23c86731661bdac8d1
Rahul Kumar <rrahul772k@gmail.com>
2024-08-26 19:57:35 +0530

    storing itme in cart

--- models/user.js
+++ models/user.js
@@ -1,18 +1,36 @@
 const db = require('../util/database').getDb
 const mongodb = require('mongodb');
+const { getDb } = require('../util/database');
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save(){
     const db = getDb();
     return db.collection('user').insertOne(this)
   }
+
+  addToCart(product){
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = {items: [{productId: new ObjectId(product._id), quantity: 1}]};
+    const db = getDb();
+    return db
+    .collection('users')
+    .updateOne(
+      {_id: new ObjectId(this._id)}, 
+      {$set: {cart: updatedCart}}
+    );
+  }
+
 
   static findById(userId){
     const db = getDb();
